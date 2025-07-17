21f212e3a4e400ffd04d99666900a369c349aec3
yu6767-1 <yu_vista@163.com>
2025-07-17 15:33:18 +0800

    完全平方

---
+++ .venv/练习2.py
@@ -0,0 +1,7 @@
+#一个整数，它加上100后是一个完全平方数，再加上268又是一个完全平方数，请问该数是多少？
+import math
+for i in range(10000):
+    x=int(math.sqrt(i+100))
+    y=int(math.sqrt(i+100+268))
+    if x*x==i+100 and y*y==i+100+268:
+        print(i)
